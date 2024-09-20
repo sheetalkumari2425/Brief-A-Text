52ed57a41ed2391fc6016b55a7fe667b46476cfa
sheetalkumari2425 <sheetal.kumari@2425>
2024-09-20 19:21:38 +0530

    Modified dataset file name

--- src/briefAtext/components/data_validation.py
+++ src/briefAtext/components/data_validation.py
@@ -10,7 +10,7 @@ class DataValidation:
     def validate_all_files_exist(self) -> bool:
         try:
             validation_status = None
-            all_files = os.listdir(os.path.join("artifacts","data_ingestion","samsum_dataset"))
+            all_files = os.listdir(os.path.join("artifacts","data_ingestion","summarizer-data-latest"))
             for file in all_files:
                 if file not in self.config.ALL_REQUIRED_FILES:
                     validation_status = False
